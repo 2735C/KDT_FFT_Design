9c62297306484a44d7e58b621aa40aa57b6be4ae
BAESANGWON1 <165987407+BAESANGWON1@users.noreply.github.com>
2025-07-24 15:33:56 +0900

    Update counter.sv

--- Prototype/cnt/counter.sv
+++ Prototype/cnt/counter.sv
@@ -2,23 +2,26 @@
 
 module num_counter #(
     parameter NUM = 16
-)  (
+)(
     input clk,
     input rst,
     input valid,
-    output logic bfly_enable
+    output logic [1:0] bfly_enable  // 2비트 출력
 );
 
     logic [$clog2(NUM) - 1:0] cnt;
+    logic [1:0] bfly_state;
 
-    always @(posedge clk or negedge rst) begin
-        if(~rst) begin
+    always_ff @(posedge clk or negedge rst) begin
+        if (~rst) begin
             cnt <= 0;
             bfly_enable <= 0;
+            bfly_state <= 0;
         end else if (valid) begin
-            if(cnt == NUM - 1) begin
+            if (cnt == NUM - 1) begin
                 cnt <= 0;
-                bfly_enable <= 1;
+                bfly_state <= bfly_state + 1;  // 0→1→2→3→0...
+                bfly_enable <= bfly_state + 1;
             end else begin
                 cnt <= cnt + 1;
             end
