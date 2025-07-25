3cc018c30a299d74b53c4c85ab86fc951f7e1d92
2735C <165992922+2735C@users.noreply.github.com>
2025-07-25 12:24:57 +0900

    Update fft_cbfp_sr.sv

--- Prototype/shift_register/fft_cbfp_sr.sv
+++ Prototype/shift_register/fft_cbfp_sr.sv
@@ -28,10 +28,10 @@ module cbfp_sr #(
             for(i=0; i<=buffer_depth; i=i+1)
                 buffer_din[i] <= 0;
         end else if (valid_in) begin
-            for (int i = buffer_depth - 1; i >= array_size; i--) begin
+            for (int i = buffer_depth - 1; i >= array_size; i = i-1) begin
                 buffer_din[i] <= buffer_din[i - array_size];
             end
-            for (int i = 0; i < array_size; i++) begin
+            for (int i = 0; i < array_size; i = i+1) begin
                     buffer_din[i] <= din[i];
             end
         end
@@ -40,17 +40,17 @@ module cbfp_sr #(
     // 출력 단계: 16개씩 순차 출력
 always_ff @(posedge clk or negedge rstn) begin
     if (!rstn) begin
-        for (int i = 0; i < array_size; i++)
+        for (int i = 0; i < array_size; i = i+1)
             dout[i] <= '0;
         cnt <= clk_cnt;
     end else if (!valid_in) begin  // 출력 phase라고 가정
-        for (int i = 0; i < array_size; i++) begin
+        for (int i = 0; i < array_size; i = i+1) begin
             dout[i] <= buffer_din[cnt * array_size + i];
         end
         cnt <= cnt - 1;
         valid_out <= 1;
     end else begin
-        for (int i = 0; i < array_size; i++)
+        for (int i = 0; i < array_size; i = i+1)
             dout[i] <= '0;
         cnt <= clk_cnt;
         valid_out <= 0;
